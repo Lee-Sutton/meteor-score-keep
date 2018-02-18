659ad8222382ccd307089e9c4cef123c463a026a
Lee Sutton <leesutton1@gmail.com>
2018-02-17 16:13:47 -0800

    Rendered players in db upon client connection
    * Client syncs with server via Autorun

--- client/main.js
+++ client/main.js
@@ -1,58 +1,55 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Meteor} from 'meteor/meteor';
-import {Tracker} from 'meteor/tracker';
-import {Players} from './../imports/api/players';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { Players } from './../imports/api/players';
 
-Tracker.autorun(function() {
-    console.log('Players list', Players.find().fetch());
-});
 
-/**
- * Player class
- */
+/** 
+ * Player class represents each player on the scoreboard
+*/
 class Player {
-    constructor(playerId, name, score = 0) {
-        this._id = playerId
-        this.name = name;
-        this.score = score;
-    }
-
-    get playerId() {
-        return this._id;
-    }
-
-    set playerId(val) {
-        this._id = val;
-    }
+  constructor(playerId, name, score = 0) {
+    this._id = playerId
+    this.name = name;
+    this.score = score;
+  }
 }
 
 /**
- * Players array
+ * 
+ * @param {[Player]} playersList renders the input playersList to jsx paragraphs
  */
-const players = [
-    new Player(1, 'Lee', 99),
-    new Player(2, 'John', -1),
-    new Player(3, 'Andrew', -12),
-];
-
 const renderPlayers = (playersList) => {
-    return playersList.map((player) => {
-        return <p key={player.playerId}>{player.name}</p>
-    });
+  return playersList.map((player) => {
+    return <p key={player._id}>{player.name} has {player.score} points</p>
+  });
 };
 
-Meteor.startup(function() {
-  let title = 'Account Settings';
-  let name = 'Lee';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      <p>This is from {name}</p>
-      <p>This is my second</p>
-      {renderPlayers(players)}
-    </div>
-  );
+/**
+* Initialize the players array upon startup
+*/
+Meteor.startup(function () {
+  console.log('Adding a player');
+  Players.insert({
+    name: 'client',
+    score: -12
+  });
+  Tracker.autorun(function () {
+    let players = Players.find().fetch();
+    let title = 'Account Settings';
+    let name = 'Lee';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>This is from {name}</p>
+        <p>This is my second</p>
+        {renderPlayers(players)}
+      </div>
+    );
 
-  ReactDom.render(jsx, document.getElementById('app'));
+    ReactDom.render(jsx, document.getElementById('app'));
+
+  });
 });
+
--- server/main.js
+++ server/main.js
@@ -1,11 +1,7 @@
-import {Meteor} from 'meteor/meteor';
-import {Players} from './../imports/api/players';
+import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
 
 Meteor.startup(function () {
-    Players.insert({
-        name: 'Ali',
-        score: 12
-    });
-    console.log(Players.find().fetch());
+  console.log(Players.find().fetch());
 });
 
