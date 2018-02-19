d003b875ae90967b7713da9ab1913a7cd3a72977
Lee Sutton <leesutton1@gmail.com>
2018-02-19 10:19:54 -0800

    Created player react component

--- client/main.js
+++ client/main.js
@@ -5,18 +5,8 @@ import { Tracker } from 'meteor/tracker';
 import { Players } from './../imports/api/players';
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
+import Player from './../imports/ui/Player'
 
-
-/** 
- * Player class represents each player on the scoreboard
-*/
-class Player {
-  constructor(playerId, name, score = 0) {
-    this._id = playerId
-    this.name = name;
-    this.score = score;
-  }
-}
 
 /**
  * 
@@ -24,25 +14,9 @@ class Player {
  */
 const renderPlayers = (playersList) => {
   return playersList.map((player) => {
-    return (
-    <p key={player._id}>
-    {player.name} has {player.score} points
-
-    <button onClick={() => {
-      Players.update(player._id, {$inc: {score: 1}})
-    }}>
-    +</button>
-    <button onClick={() => {
-      Players.update(player._id, {$inc: {score: -1}})}
-    }>
-    -</button>
-    <button onClick={() => Players.remove(player._id)}>X</button>
-    </p>
-  );
+    return <Player key={player._id} player={player}/>;
   });
 };
-
-
 
 /**
 * Initialize the players array upon startup
@@ -56,7 +30,7 @@ Meteor.startup(() => {
       <div>
         <TitleBar title={title}/>
         {renderPlayers(players)}
-        <AddPlayer score={10}/>
+        <AddPlayer/>
       </div>
     );
     ReactDom.render(jsx, document.getElementById('app'));
